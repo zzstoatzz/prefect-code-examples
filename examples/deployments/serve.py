d8b97a487a361801847f85ad9ab60f5756ebf948
Nathan Nowack <thrast36@gmail.com>
2024-02-17 04:47:33 -0600

    simplify

--- examples/deployments/serve.py
+++ examples/deployments/serve.py
@@ -1,7 +1,5 @@
-import json
 import time
 from prefect import flow, task
-from prefect.artifacts import create_markdown_artifact
 from pydantic import BaseModel, Field
 
 class Data(BaseModel):
@@ -24,20 +22,9 @@ def process_data(data: Data) -> Data:
 def work_i_need_to_do_sometimes(keys: list[str]):
     """Work you might need to do ever so often"""
     
-    keys_repr = "-".join(sorted(keys))
-    
     raw_data_futures = fetch_data_for_key.map(keys)
 
-    futures = process_data.map(raw_data_futures)
-    
-    create_markdown_artifact(
-        markdown=(
-            "![image](https://t.ly/xsf5J)\n"
-            f"## processed data for {keys_repr}\n\n```json\n"
-            f"{json.dumps([f.result().model_dump() for f in futures], indent=2)}\n```"
-        ),
-        key=keys_repr,
-    )
+    process_data.map(raw_data_futures)
 
 if __name__ == "__main__":
     work_i_need_to_do_sometimes.serve(
